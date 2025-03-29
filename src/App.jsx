29afc52ec49e64129b428ffb907bf351c17acab4
Marko Tsymbaluk <markotsymbaluk@gmail.com>
2025-03-29 10:50:46 -0400

    ADD: Images are retrieved

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
+
+const API_KEY = "";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const getRandomDog = async () => {
+    const response = await fetch(
+      `https://api.thedogapi.com/v1/images/search?&api_key=${API_KEY}`
+    );
+    const data = await response.json();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getRandomDog();
+  }, []);
+
+  return <></>;
 }
 
-export default App
+export default App;
